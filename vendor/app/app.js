1a97a25eb733f196c1cafd8378a7255d97a7eb7d
ProudToBe_Dev <wisedsri2@gmail.com>
2017-12-26 11:38:20 +0700

    Update UI And Function User Added

--- vendor/app/app.js
+++ vendor/app/app.js
@@ -1,18 +1,17 @@
-// Initialize Firebase
-var config = {
-    apiKey: "AIzaSyACWpYN8fQJux5WYILmvegefxGjKgz0Ikc",
-    authDomain: "sutsign-9602f.firebaseapp.com",
-    databaseURL: "https://sutsign-9602f.firebaseio.com",
-    projectId: "sutsign-9602f",
-    storageBucket: "sutsign-9602f.appspot.com",
-    messagingSenderId: "670407673317"
-};
+	// Initialize Firebase
+	var config = {
+	    apiKey: "AIzaSyACWpYN8fQJux5WYILmvegefxGjKgz0Ikc",
+	    authDomain: "sutsign-9602f.firebaseapp.com",
+	    databaseURL: "https://sutsign-9602f.firebaseio.com",
+	    projectId: "sutsign-9602f",
+	    storageBucket: "sutsign-9602f.appspot.com",
+	    messagingSenderId: "670407673317"
+	};
 
-firebase.initializeApp(config);
+	firebase.initializeApp(config);
 
 	/* Function Search From Student id and Course */
 	function search_signs(course_id,student_code) {
-
 		if(course_id == '' || student_code == ''){
 			alert('กรุณากรอกข้อมูล รหัสวิชา และ รหัสนักศึกษา !');
 		}else{
@@ -21,25 +20,9 @@ firebase.initializeApp(config);
 
 	}
 
-	// Tests to see if /users/<userId> has any data. 
-	/* function checkIfCourseExists(course_id,student_code) {
-		$.LoadingOverlay("show");
-		// Firebase Ref
-		const rootRef = firebase.database().ref();
-		const oneRef = rootRef.child('Student').child(student_code).child(course_id);
-
-		oneRef.once('value', function(snapshot) {
-			var exists = (snapshot.val() !== null);
-			courseDataExistsCallback(course_id, exists,student_code);
-		});
-
-	} */
-
+	/* ตรวจสอบว่ามีรายวิชาและชื่อนี้อยู่หรือไม่  */
 	function checkIfCourseExists(course_id, student_code) {
-		
-		//$.LoadingOverlay("show");
-		
-		// Firebase Ref
+
 		const rootRef = firebase.database().ref();
 		const oneRef = rootRef.child('Student').child(student_code).child(course_id);
 
@@ -51,100 +34,54 @@ firebase.initializeApp(config);
 
 	/* Check If Query Have Data */
 	function courseDataExistsCallback(course_id, exists, student_code) {
-		
-	if (exists) {
-
-		$("#signShow").LoadingOverlay("show");
-
-		const rootRef = firebase.database().ref();
-		$("#signShow").find("tr:gt(0)").remove();
-
-
-		firebase.database().ref("/Student").child(student_code + '/' + course_id).on("value", function (snapshot) {
-			snapshot.forEach(function (childSnapshot) {
-
-				console.log(childSnapshot.key, childSnapshot.val());
-				console.log(childSnapshot.child('Sign 1').val());
-
-				$('#signShow').append('<tr> <td> ' + childSnapshot.key + ' </td> <td> ' + childSnapshot.child('Sign 1').val() + '</td> <td> ' + childSnapshot.child('Sign 2').val() + ' </td> <td> ' + childSnapshot.child('Sign 3').val() + ' </td> <td> ' + childSnapshot.child('Sign 4').val() + ' </td> <td> E-Mail </td> </tr>' );
-				
-				//appendSignRow(childSnapshot.key, childSnapshot.val());
+		if (exists) {
+			getCourseName(course_id);
+			$("#signShow").LoadingOverlay("show");
+			$("#signShow").find("tr:gt(0)").remove();
+			var i = 0;
+			const rootRef = firebase.database().ref();
+			firebase.database().ref("/Student").child(student_code + '/' + course_id).on("value", function (snapshot) {
+				snapshot.forEach(function (childSnapshot) {
+					$('#signShow').append('<tr> <td> ' + childSnapshot.key + ' </td> <td> ' + childSnapshot.child('Sign 1').val() + '</td> <td> ' + childSnapshot.child('Sign 2').val() + ' </td> <td> ' + childSnapshot.child('Sign 3').val() + ' </td> <td> ' + childSnapshot.child('Sign 4').val() + ' </td> <td> <button id="user_'+ i +'" type="button" class="btn btn-default" data-toggle="tooltip" data-placement="right" title="'+ childSnapshot.child('updated_by').val() +'" onmouseover="tooltip_show(this.id);"> <i class="fa fa-lg fa-user-circle"></i> </button> </td> </tr>' );
+					i++;
+				});
 			});
-		});
-
-
-		$("#signShow").LoadingOverlay("hide");
-
-		/* function appendSignRow(count, signs, course_id) {
-
-			var trHTML = '<tr><td>' + count + '</td>';
-			$.each(signs, function (i, item) {
-				trHTML += '<td>' + item + '</td>';
-			});
-			$('#signShow').append(trHTML + '</tr>');
-			//checkHideOverlayLoading(count , course_id);
-			//$.LoadingOverlay("hide");
-		} */
-
-		/* const rootRef = firebase.database().ref();
-		const oneRef = rootRef.child('Student').child(student_code).child(course_id);
-
-		// Query Data
-		var count = 1;
-		// Remove AlL Row Before Append
-		$("#signShow").find("tr:gt(0)").remove();
-
-		oneRef.on('child_added', snap => {
-			//console.log(snap.val());
-			appendSignRow(count , snap.val(), course_id);
-			count++;
-		}); */
-
-		//getCourseName(course_id);
-
-	} else {
-		//$.LoadingOverlay("hide");
-		alert('กรอกข้อมูล รหัสวิชา หรือ รหัสนักศึกษาไม่ถูกต้อง !');
+			$("#signShow").LoadingOverlay("hide");
+		} else {
+			alert('กรอกข้อมูล รหัสวิชา หรือ รหัสนักศึกษาไม่ถูกต้อง !');
+		}
 	}
-	}
-
 
 	/* Get Course Name */
-	/* function getCourseName(course_id){
-
-		const rootRef = firebase.database().ref();
-		const oneRef = rootRef.child('Course/' + course_id + '/description');
-
-		oneRef.on('child_added', snap => {
-			//$("#showCourseName").html(snap);
-			//console.log(snap);
-			if(snap.key == 'name'){
-				console.log(snap.val());
-				$("#showCourseName").html(snap.val());
-				$.LoadingOverlay("hide");
-			}
+	 function getCourseName(course_id){
+	 	const rootRef = firebase.database().ref();
+	 	firebase.database().ref('Course/' + course_id + '/description').on("value", function (snapshot) {
+	 		console.log( snapshot.val().name );
+			$("#showCourseName").html( snapshot.val().name );
 		});
 	}
-	*/
+	
+	$(function () {
+	  $('[data-toggle="tooltip"]').tooltip()
+	})
 
-
+	function tooltip_show(id)
+    {	
+        $('#'+id).tooltip('show');
+    } 
 
 	$("#text").click(function () {
 		makeCode();
 	});
 
-
 	var qrcode = new QRCode("qrcode");
 	function makeCode () {   
-
 		var elText = document.getElementById("student_code");
-		
 		if (!elText.value) {
 			alert("กรุณากรอกรหัสนักศึกษา");
 			elText.focus();
 			return;
 		}
-		
 		qrcode.makeCode(elText.value);
 	}
 
